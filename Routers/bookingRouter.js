b11a33896d4f504ee2a0f688d8d6211a69289b4d
Nayan Chandra Datta <nayanchdatta011@gmail.com>
2022-11-16 23:52:12 +0600

    some api changes made

--- Routers/bookingRouter.js
+++ Routers/bookingRouter.js
@@ -7,7 +7,7 @@ const Booking = mongoose.model('Booking', bookingSchema);
 // Get all bookings 
 bookingRouter.get('/', async (req, res) => {
     try {
-        const query = {};
+        const query = { patientEmail: req.query.email };
         const projection = { __v: 0 };
         const bookings = await Booking.find(query, projection);
         res.status(200).json({ success: true, bookings });
@@ -19,9 +19,15 @@ bookingRouter.get('/', async (req, res) => {
 // Booking api for POST 
 bookingRouter.post('/', async (req, res) => {
     try {
-        const booking = new Booking(req.body);
-        const bookingCollection = await booking.save();
-        res.status(200).json({ success: true, booking: bookingCollection });
+        const query = { treatment: req.body.treatment, date: req.body.date, patientEmail: req.body.patientEmail };
+        const exists = await Booking.findOne(query);
+        if (!exists) {
+            const booking = new Booking(req.body);
+            const bookingCollection = await booking.save();
+            res.status(200).json({ success: true, booking: bookingCollection });
+        } else {
+            res.status(500).json({ success: false, message: 'Already booked for a treatment on' });
+        }
     } catch (err) {
         res.status(500).json({ success: false, message: 'Error occured on inserting a document.' });
     }
